c93f4f36cbb3b6ef6e2db65cb3dc741d28bb96e7
Nirbhay Meghpara <nirbhaymeghpara123@gmail.com>
2025-04-05 01:08:27 -0300

    refactor: remove unused winston-transport dependency and custom WebSocket transport from logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,18 +1,5 @@
 import { createLogger, format, transports } from 'winston';
-import Transport from 'winston-transport';
 import { config } from '../config/config.js';
-import { broadcastLog } from '../websocket.js';
-
-// Custom WebSocket Transport
-class WebSocketTransport extends Transport {
-  constructor() {
-    super();
-  }
-  log(info: any, callback: () => void) {
-    broadcastLog({ message: info.message, env_name: info.env_name || 'unknown', timestamp: info.timestamp });
-    callback();
-  }
-}
 
 const logger = createLogger({
   level: config.env === 'development' ? 'debug' : 'info',
@@ -22,7 +9,6 @@ const logger = createLogger({
   ),
   transports: [
     new transports.Console(),
-    new WebSocketTransport(),
   ],
 });
 
